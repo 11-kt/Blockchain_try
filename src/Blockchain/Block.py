02bec353af8e00fc4ded856988d927d06103be47
Andrianov Vitaly <andrianov.vital@yandex.ru>
2023-04-12 00:42:00 +0300

    #1 Add Block and create hash

--- src/Blockchain/Block.py
+++ src/Blockchain/Block.py
@@ -1,4 +1,6 @@
+import json
 import time
+from hashlib import sha256
 
 
 class Block:
@@ -15,7 +17,25 @@ class Block:
         # nonce
         self.nonce = 0
         # Текущий хэш блока
-        self.hash = None
+        self.hash = self.get_hash()
         # Время выпуска блока
         self.timestamp = time.time()
 
+    # Вычисление текущего хэша блока
+    def get_hash(self):
+        h = sha256()
+        string = (str(self.index) + str(self.prev_hash) + str(self.data) + str(self.nonce)).encode('utf-8')
+        h.update(string)
+        return h.hexdigest()
+
+    def __repr__(self):
+        return json.dumps([
+            {
+                'From node': self.num_node,
+                'data': self.data,
+                'index': self.index,
+                'nonce': self.nonce,
+                'hash': self.hash,
+                'prev_hash': self.prev_hash,
+                'timestamp': self.timestamp
+            }], indent=4)
