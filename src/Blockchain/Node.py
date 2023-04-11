485c221a67642ff91082284ccd11f9d9a3d01f14
Andrianov Vitaly <andrianov.vital@yandex.ru>
2023-04-12 00:55:22 +0300

    #2 Create Node create genesis and generate new block

--- src/Blockchain/Node.py
+++ src/Blockchain/Node.py
@@ -28,3 +28,38 @@ class Node:
         else:
             self.cur_nonce += random.randint(10, 15)
 
+    # Создание нового блока
+    def create_new_block(self, num_node):
+        current_block = Block(random_string(256), num_node)
+        current_block.index = self.get_last_block().index + 1
+        current_block.prev_hash = self.get_last_block().hash
+        current_block.nonce = 0
+        self.cur_nonce = 0
+        current_block.hash = current_block.get_hash()
+
+        # Пока не выполняется требование к точности, генерируем новый блок
+        while current_block.hash[-4:] != '0' * self.diff:
+            if len(self.chain) > 1 and self.get_last_block().index == current_block.index:
+                return None
+            self.add_nonce()
+            current_block.nonce = self.cur_nonce
+            current_block.hash = current_block.get_hash()
+
+        # Время создания блока
+        current_block.timestamp = time.time()
+
+        return current_block
+
+    # Создание genesis
+    def create_first_block(self, num_node):
+        data = random_string(256)
+        first_hash = create_hash(1, None, data, 0)
+        while first_hash[-4:] != '0' * self.diff:
+            data = random_string(256)
+            first_hash = create_hash(1, None, data, 0)
+
+        block = Block(data, num_node)
+        block.index = 1
+        block.hash = block.get_hash()
+        self.chain.append(block)
+
