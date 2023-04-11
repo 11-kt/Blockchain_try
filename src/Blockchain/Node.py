50a484557f93cb2f7c86244618a9fd17dc0f187d
Andrianov Vitaly <andrianov.vital@yandex.ru>
2023-04-12 00:54:07 +0300

    #2 Create Node add get_last_block and nonce

--- src/Blockchain/Node.py
+++ src/Blockchain/Node.py
@@ -1,3 +1,7 @@
+import json
+import time
+from Blockchain.utils import *
+from Blockchain.Block import Block
 
 
 class Node:
@@ -10,4 +14,17 @@ class Node:
         self.cur_nonce = 0
         # Тип вычисления nonce
         self.nonce_type = nonce_type
+
+    # Получение последнего блока цепи
+    def get_last_block(self):
+        return self.chain[len(self.chain) - 1]
+
+    # Генерация следующего значения nonce
+    def add_nonce(self):
+        if self.nonce_type == 0:
+            self.cur_nonce += random.randint(0, 5)
+        elif self.nonce_type == 1:
+            self.cur_nonce += random.randint(5, 10)
+        else:
+            self.cur_nonce += random.randint(10, 15)
 
